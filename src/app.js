536bf948206e38081083fc161715af80c05aeb65
Raphael Malka <raphael.malka@student.vinci.be>
2023-01-20 10:54:50 +0100

    refractor app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 /* istanbul ignore file */
 try {
     const { isEmpty } = require("./services/validations");
+    const { checkGamerTag } = require("./services/validations");
     const gamerTagInput = document.getElementById("gamerTagInput");
     const checkButton = document.getElementById("gamerTagCheckButton");
     const feedbackMessage = document.getElementById("gamerTagFeedback");
@@ -14,9 +15,14 @@ try {
 
     checkButton.addEventListener("click", () => {
         // TODO: Add the logic to display the correct feedback message (error and validation)
-        feedbackMessageText = isEmpty(gamerTagValue)
-            ? "Gamer tag cannot be empty"
-            : "Gamer tag is valid";
+        if(feedbackMessageText = isEmpty(gamerTagValue)){
+            feedbackMessageText = "Gamer tag cannot be empty";
+        }
+        if(checkGamerTag(gamerTagValue) === true){
+            feedbackMessageText = "Gamer tag is valid";
+        }else{
+            feedbackMessageText = "Gamer tag is invalid";
+        }
         feedbackMessage.textContent = feedbackMessageText;
     });
 } catch (err) {
