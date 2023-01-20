f45c86bc63e9e64d5d20ad6888c5f062ac752ed8
Raphael Malka <raphael.malka@student.vinci.be>
2023-01-20 09:52:41 +0100

    first test implemented : check Tagname lenght

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -1,4 +1,5 @@
 const { isEmpty } = require("../validations");
+const { checkGamerTag } = require("../validations");
 
 describe("validations tests suites - isEmpty", () => {
     test("should return true as the label is undefined", () => {
@@ -18,3 +19,12 @@ describe("validations tests suites - isEmpty", () => {
 });
 
 // TODO: Create tests suite for validation function
+
+
+describe("Check Gamer Tag test that check if the tag is correct ", () => {
+    test("should return True if Tag name have a minimum of 8 caracter", () => {
+        const result = checkGamerTag();
+        expect(result).toBe(true);
+    });
+
+});
--- src/services/validations.js
+++ src/services/validations.js
@@ -2,4 +2,9 @@
 
 const isEmpty = (label) => !label || label.length === 0;
 
+function checkGamerTag() {
+    return false;
+}
+
 exports.isEmpty = isEmpty;
+exports.checkGamerTag = checkGamerTag;
